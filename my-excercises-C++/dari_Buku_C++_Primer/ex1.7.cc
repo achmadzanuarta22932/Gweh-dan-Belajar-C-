cb6c8347b144be81340b39181da59be26b31f00a
AchmadZanuarta <achmadzanuarta@gmail.com>
2026-08-09 20:50:27 +0700

    Add files via upload

---
+++ my-excercises-C++/dari_Buku_C++_Primer/ex1.7.cc
@@ -0,0 +1,12 @@
+int main() {
+
+    /** ini di luar 
+    * /* ini di dalam (nested) */
+    /* ini komen di luar lagi  // but if aded  it is like this 
+    */
+
+    // it is like this /* put any thing inside this and close with this */
+    // example 
+
+    /* /* */ */ /* */
+}
